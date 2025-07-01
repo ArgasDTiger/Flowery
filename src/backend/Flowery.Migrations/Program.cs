c54a63f96911435f5b18962769299c88b4b52727
Argas <bodia02981@gmail.com>
2025-07-01 21:38:38 +0300

    add migrations project and first migration script

---
+++ src/backend/Flowery.Migrations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddCommandLine(args)
+    .Build();
+    
+var environmentName = config["environment"];
+
+var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+var myConfig = new ConfigurationBuilder()
+    .SetBasePath(basePath) // Ensures the path is where the DLL/EXE lives
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+    .Build();
+
+
+Console.WriteLine(myConfig["ConnectionStrings:Postgres"]);
--- src/backend/Flowery.WebApi/Entities/User.cs
+++ src/backend/Flowery.WebApi/Entities/User.cs
@@ -3,8 +3,10 @@
 public sealed class User
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
     public string PasswordSalt { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
     public UserRole Role { get; set; } = null!;
