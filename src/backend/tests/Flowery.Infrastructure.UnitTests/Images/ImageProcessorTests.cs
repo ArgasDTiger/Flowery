d635632079475b0f2ed671ce8d3bed34f1fbce30
Bohdan <bodia02981@gmail.com>
2026-02-21 15:49:19 +0200

    started writing unit tests for the infra, but nah, I'm too lazy to play with extension methods substition for now

---
+++ src/backend/tests/Flowery.Infrastructure.UnitTests/Images/ImageProcessorTests.cs
@@ -0,0 +1,26 @@
+﻿using SixLabors.ImageSharp;
+using Flowery.Infrastructure.Images;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Metadata;
+
+namespace Flowery.Infrastructure.UnitTests.Images;
+
+public sealed class ImageProcessorTests
+{
+    [Fact]
+    public async Task SaveOriginal_ShouldReturnExpectedPath()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource();
+        var image = Substitute.For<Image>(new Configuration(), new PixelTypeInfo(0), new ImageMetadata(), new Size());
+        image.SaveAsync(Arg.Any<string>(), cts.Token).Returns(Task.CompletedTask);
+
+        var imageProcessor = new ImageProcessor();
+        // Act
+
+        string path = await imageProcessor.SaveOriginal(image, "name", "extension", "path", cts.Token);
+        // Assert
+        
+        path.ShouldBe("/images/path/originals/name.extension");
+    }
+}
